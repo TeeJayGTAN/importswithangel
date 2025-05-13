e78f15030aa3ad969f5d1b3f90dc42d8787afa84
TeeJayGTAN <oniretemisan@gmail.com>
2025-05-13 09:09:22 +0100

    iwa

--- app/page.tsx
+++ app/page.tsx
@@ -5,46 +5,107 @@ export default function Home() {
     <div className="w-full">
       <header className="font-medium text-sm tracking-tighter w-full bg-black text-white flex justify-center py-4 items-center">Imports With Angel</header>
       <section className="px-4 hero w-full flex flex-col items-center ">
-        <div className="headline font-bold mt-18 leading-12 text-4xl text-center w-full">Import from China like a pro</div>
-        <div className="subheadline text-base pt-7 font-medium text-center w-full">Get insider knowledge on navigating China's complex market, finding reliable suppliers, and streamlining your import process to boost profits and growth.</div>
+        <div className="headline font-bold mt-18 leading-12 text-4xl text-center w-full">Learn How to Import Products from China Without Getting Scammed — Even If You’re a Total
+Beginner</div>
+        <div className="subheadline text-base pt-7 font-medium text-center w-full"> Join 1,500+ Nigerians making 2x–5x profit reselling products they import directly from China. ₦3,000 Promo Ends in 5 Days! (Then the price goes back to ₦10,000).</div>
         <div className="video">
           <h3 className="bg-green-200 mt-5 mb-5 font-medium text-sm p-4 text-center rounded"> <span className="text-lg text-green-700">Step 1 of 2: </span>Watch This Video</h3>
           <video  className="rounded" loop controls>
             <source src="/vid.mp4" type="video/mp4"/>
           </video>
         </div>
-        <button className="cta w-full mt-5 font-medium text-sm p-4 rounded bg-red-400"><span className="text-lg text-red-700">Step 2 of 2: </span>Click here to ENROLL NOW</button>
+        <button className="cta w-[50%] mt-5 font-medium text-sm p-4 rounded bg-red-400"><span className="text-lg text-red-700">Step 2 of 2: </span><br/>Click here to Join the Class Now — Only ₦3,000</button>
+      {/* put timer component here */}
       </section>
-      <section className="w-full mt-18 mb-18 px-4">
-        <h3 className="font-bold leading-12 text-4xl text-center w-full capitalize">What you'll get in this course</h3>
-        <h5 className="text-sm mt-7 text-center mb-8">You'll Learn All you need to know about importing from china, Identify Quality suppliers and spot fake vendors that will scam you. From placing orders from quality suppliers to shipping, you will be walked through the importation process step by step.</h5>
+      <section className="px-4 hero w-full flex flex-col items-center my-18">
+        {/* pain and promise section */}
+        <div className="font-bold leading-12 text-4xl text-center w-full capitalize mb-12">
+          Struggling to find a real side hustle that works in Nigeria?
+        </div>
+         <ul>
+          <li className="flex justify-start items-center"><img className="w-[32px]" src="bullet.png"/>Are you tired of buying from Nigerian vendors and making tiny profits?.</li>
+          <li className="flex justify-start items-center"><img className="w-[32px]" src="bullet.png"/>Are you scared of losing money to scammers in China?</li>
+          <li className="flex justify-start items-center"><img className="w-[32px]" src="bullet.png"/>Confused about shipping, agents, and product sourcing?</li>
+         </ul>
+        <h5 className="text-sm mt-7 text-center mb-8">This class is for you. In just 5 days, I’ll show you how to:</h5>
         <ul>
-          <li className="flex justify-start items-center"><img className="w-[32px]" src="check-mark.png"/>Access to all course materials 24/7.</li>
-          <li className="flex justify-start items-center"><img className="w-[32px]" src="check-mark.png"/>Study when it suits your schedule.</li>
-          <li className="flex justify-start items-center"><img className="w-[32px]" src="check-mark.png"/>Engage with discussion forums and peer collaboration opportunities</li>
-          <li className="flex justify-start items-center"><img className="w-[32px]" src="check-mark.png"/>Complete assignments and quizzes at your own pace.</li>
-          <li className="flex justify-start items-center"><img className="w-[32px]" src="check-mark.png"/>Close Mentorship from me throughout your journey.</li>
-        </ul>
+          <li className="flex justify-start items-center"><img className="w-[32px]" src="bullet.png"/>Import products safely from China (without travel or scams)</li>
+          <li className="flex justify-start items-center"><img className="w-[32px]" src="bullet.png"/>Get direct access to trusted agents & suppliers</li>
+          <li className="flex justify-start items-center"><img className="w-[32px]" src="bullet.png"/>Start with as little as ₦50k and scale fast</li>
+          <li className="flex justify-start items-center"><img className="w-[32px]" src="bullet.png"/>Resell and make 200%+ profit like my other students</li>
+         </ul>
+         <button className="cta w-[50%] mt-5 font-medium text-sm p-4 rounded bg-red-400">Click here to Join the Class Now — Only ₦3,000</button>
       </section>
       <section className="mb-18 px-4 w-full flex flex-col items-center text-center testimonials bg-gray-100 py-18">
-        <h3 className="font-bold leading-12 text-4xl text-center w-full capitalize">Client Testimonials</h3>
-        <h5 className="text-sm mt-7 text-center mb-8">Here's what our customers from all over the world say about course</h5>
-        <div className="btns flex items-center justify-center gap-4">
+        <h3 className="font-bold leading-12 text-4xl text-center w-full capitalize">Real Results. Real People.</h3>
+        <h5 className="text-sm mt-7 text-center mb-8">Don’t take my word for it — here’s what past students are saying.</h5>
+        {/* <div className="btns flex items-center justify-center gap-4">
           <div className="left-arrow cursor-pointer opacity-40 border p-4 rounded-full"><img className="w-[32px]" src="/back.png" alt="" /></div>
           <div className="right-arrow cursor-pointer border p-4 opacity-80 rounded-full"><img className="w-[32px]" src="/next.png" alt="" /></div>
+        </div> */}
+        <div className="testimony-grid grid grid-cols-1 md:grid-cols-3 gap-4">
+          <div className="testimony mt-10 py-12 mx-auto w-full flex flex-col gap-y-4 justify-center items-center bg-white rounded">
+            <div className="profile-pic w-[286px] rounded">
+              <img className="w-full overflow-hidden rounded" src="/R1.jpg" alt="testimony pic" />
+            </div>
+            {/* <div className="review-stars w-[80px]">
+              <img className="w-full" src="/star.png" alt="" />
+            </div> */}
+            <div className="testimonial px-4">
+              The course offered valuable insights and clear steps for success. The tips were easy to implement, and I've already noticed significant engagement improvements. One of the best courses!
+            </div>
+            <div className="testimonial-name mt-6 text-lg font-bold">Monica Davies</div>
+            <div className="text-xs testimonial-role pt-2">- CEO Fashion Wears Store</div>
+          </div>
+          <div className="testimony mt-10 py-12 mx-auto w-full flex flex-col gap-y-4 justify-center items-center bg-white rounded">
+            <div className="profile-pic w-[286px] rounded">
+              <img className="w-full overflow-hidden rounded" src="/R1.jpg" alt="testimony pic" />
+            </div>
+            {/* <div className="review-stars w-[80px]">
+              <img className="w-full" src="/star.png" alt="" />
+            </div> */}
+            <div className="testimonial px-4">
+              The course offered valuable insights and clear steps for success. The tips were easy to implement, and I've already noticed significant engagement improvements. One of the best courses!
+            </div>
+            <div className="testimonial-name mt-6 text-lg font-bold">Monica Davies</div>
+            <div className="text-xs testimonial-role pt-2">- CEO Fashion Wears Store</div>
+          </div>
+          <div className="testimony mt-10 py-12 mx-auto w-full flex flex-col gap-y-4 justify-center items-center bg-white rounded">
+            <div className="profile-pic w-[286px] rounded">
+              <img className="w-full overflow-hidden rounded" src="/R1.jpg" alt="testimony pic" />
+            </div>
+            {/* <div className="review-stars w-[80px]">
+              <img className="w-full" src="/star.png" alt="" />
+            </div> */}
+            <div className="testimonial px-4">
+              The course offered valuable insights and clear steps for success. The tips were easy to implement, and I've already noticed significant engagement improvements. One of the best courses!
+            </div>
+            <div className="testimonial-name mt-6 text-lg font-bold">Monica Davies</div>
+            <div className="text-xs testimonial-role pt-2">- CEO Fashion Wears Store</div>
+          </div>
         </div>
-        <div className="testimony mt-10 py-12 mx-auto w-full flex flex-col justify-center items-center bg-white rounded">
-          <div className="profile-pic w-[286px] rounded">
-            <img className="w-full overflow-hidden rounded" src="/woman.jpg" alt="profile pic" />
-          </div>
-          <div className="review-stars w-[80px]">
-            <img className="w-full" src="/star.png" alt="" />
-          </div>
-          <div className="testimonial px-4">
-            The course offered valuable insights and clear steps for success. The tips were easy to implement, and I've already noticed significant engagement improvements. One of the best courses!
-          </div>
-          <div className="testimonial-name mt-6 text-lg font-bold">Monica Davies</div>
-          <div className="text-xs testimonial-role pt-2">- CEO Fashion Wears Store</div>
+      </section>
+      <section className="w-full mt-18 mb-18 px-4">
+        <h3 className="font-bold leading-12 text-4xl text-center w-full capitalize">What you'll learn in this class</h3>
+        <h5 className="text-sm mt-7 text-center mb-8">Inside the Importation Masterclass, You’ll Discover:</h5>
+        <ul>
+          <li className="flex justify-start items-center"><img className="w-[32px]" src="check-mark.png"/>How to find trusted China suppliers.</li>
+          <li className="flex justify-start items-center"><img className="w-[32px]" src="check-mark.png"/>How to calculate total cost (products, shipping, clearing).</li>
+          <li className="flex justify-start items-center"><img className="w-[32px]" src="check-mark.png"/>How to talk to agents (with a template).</li>
+          <li className="flex justify-start items-center"><img className="w-[32px]" src="check-mark.png"/>Products to AVOID so you don’t lose money.</li>
+          <li className="flex justify-start items-center"><img className="w-[32px]" src="check-mark.png"/>20 HOT-SELLING items Nigerians buy like crazy.</li>
+          <li className="flex justify-start items-center"><img className="w-[32px]" src="check-mark.png"/>How to receive your items safely.</li>
+          <li className="flex justify-start items-center"><img className="w-[32px]" src="check-mark.png"/>BONUS: Access to my 3 trusted agents & 2 product sourcing plug-ins.</li>
+          <li className="flex justify-start items-center"><img className="w-[32px]" src="check-mark.png"/>BONUS: My personal supplier list.</li>
+          <li className="flex justify-start items-center"><img className="w-[32px]" src="check-mark.png"/>BONUS: Agent contact script (no begging or fear).</li>
+          <li className="flex justify-start items-center"><img className="w-[32px]" src="check-mark.png"/>BONUS: Live Zoom call access for Q&A.</li>
+          <li className="flex justify-start items-center"><img className="w-[32px]" src="check-mark.png"/>BONUS: Certificate of Completion.</li>
+        </ul>
+        
+        <h5 className="text-sm mt-7 text-center mb-8">All for just ₦3,000(for a limited time) After 5 days, the price goes back to ₦10,000</h5>
+        {/* TIMER COMPONENET HERE */}
+        <div className="cont w-full flex justify-center">
+          <button className="cta w-[50%] mt-5 font-medium text-sm p-4 rounded bg-red-400">Click here to Join the Class Now — Only ₦3,000</button>
         </div>
       </section>
 
@@ -67,7 +128,7 @@ export default function Home() {
         </div>
       </section>
       
-      <section className="pricing mb-18 px-4 w-full flex flex-col items-center text-center testimonials bg-gray-100 py-18">
+      {/* <section className="pricing mb-18 px-4 w-full flex flex-col items-center text-center testimonials bg-gray-100 py-18">
         <h3 className="font-bold leading-12 text-4xl text-center w-full capitalize">Enroll Now</h3>
         <div className="pricing-plan-pill gap-6 px-4 mt-10 py-12 mx-auto w-full flex flex-col justify-center items-center bg-white rounded">
           <h5 className="text-[#3a3a3a] text-2xl font-bold text-left self-start">Get Started</h5>
@@ -85,21 +146,21 @@ export default function Home() {
         </div>
         <button className="bg-red-400 mt-2 p-4 rounded ">ENROLL NOW</button>
         </div>
-      </section>
+      </section> */}
 
       <section className="px-4 mb-18">
         <h3 className= "font-bold leading-12 text-4xl text-center w-full capitalize mb-12">Frequently Asked Questions</h3>
         <div className="questions-accordion mt-4">
           <div className="question justify-between border-t border-gray-400 py-6 flex items-center">
-            <span className="question-text font-bold text-base max-w-[80%]">Is this course delivered live or is it pre-recorded?</span>
+            <span className="question-text font-bold text-base max-w-[80%]">Will I be scammed if I try this??</span>
             <span className="chevron-down w-[32px] block"><img className="w-full h-full object-contain" src="down.png" alt="pic" /></span>
           </div>
           <div className="question justify-between border-t border-gray-400 py-6 flex items-center">
-            <span className="question-text font-bold text-base max-w-[80%]">What is the course refund policy?</span>
+            <span className="question-text font-bold text-base max-w-[80%]">I’ve never done anything like this before. Is it beginner-friendly?</span>
             <span className="chevron-down w-[32px] block"><img className="w-full h-full object-contain" src="down.png" alt="pic" /></span>
           </div>
           <div className="question justify-between border-t border-gray-400 py-6 flex items-center">
-            <span className="question-text font-bold text-base max-w-[80%]">How long will I have access to the course materials?</span>
+            <span className="question-text font-bold text-base max-w-[80%]">When will I get access?</span>
             <span className="chevron-down w-[32px] block"><img className="w-full h-full object-contain" src="down.png" alt="pic" /></span>
           </div>
           <div className="question justify-between border-t border-gray-400 py-6 flex items-center">
@@ -115,6 +176,19 @@ export default function Home() {
             <span className="chevron-down w-[32px] block"><img className="w-full h-full object-contain" src="down.png" alt="pic" /></span>
           </div>
         </div>
+      </section>
+      <section className="px-4 mb-18">
+        <h3 className= "font-bold leading-12 text-4xl text-center w-full capitalize mb-12">Join 1,500+ Others Who’ve Learned to Import With Just ₦3,000.</h3>
+        <h5 className="text-sm mt-7 text-center mb-8">Don’t miss out. This knowledge could change your income forever.</h5>
+        <button className="cta w-full mt-5 font-medium text-sm p-4 rounded bg-red-400">Click here to Join the Class Now — Only ₦3,000</button>
+      </section>
+      
+      <section className="mb-18 px-4 w-full flex flex-col items-center text-center testimonials bg-gray-100 py-18">
+        <h3 className="font-bold leading-12 text-4xl text-center w-full capitalize">WHO IS ANGEL?</h3>
+        <h5 className="text-sm mt-7 text-center mb-8">Hi, I’m Angel, a full-time importer and trainer. I’ve helped over 1,500 Nigerians learn to import from China the RIGHT way — without stress or scams. Now it’s your turn!.</h5>
+        <div className="profile-pic w-[286px] rounded">
+              <img className="w-full overflow-hidden rounded" src="/angel.jpeg" alt="angel pic" />
+            </div>
       </section>
       <footer className="px-4 mt-18 w-full p-3 bg-black text-white" >
         <div className="w-full flex justify-center gap-x-2 items-center">
